3bc38d38d5ba54fb6eadb537a1bc14f8290ab122
Johnie Lee  Fain <jf03436@srcs.loc>
2017-03-01 08:35:06 -0800

    additional Q unit tests

--- tests.js
+++ tests.js
@@ -3,3 +3,12 @@ QUnit.test("Test the getAreaCode function.", function (assert) {
     var result = getAreaCode(num);
     assert.deepEqual(result, "415", "Valid area code test passed.");
 });
+QUnit.test( "Errors thrown for getAreaCode", function( assert ) {
+    assert.throws( function() {
+        getAreCode("415)444-5555");
+    }, "Missing '('. An error should have been thrown." );
+
+    assert.throws( function() {
+        getAreCode("(415 444-5555");
+    }, "Missing ')'. An error should have been thrown." );
+});
